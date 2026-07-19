4747d5ed27bde940d1d468e040dac2218b527299
ASHIKA22R <rashika051@gmail.com>
2026-07-19 17:25:18 +0530

    commit change

--- api/geoapify.js
+++ api/geoapify.js
@@ -1,8 +1,16 @@
 export default async function handler(req, res) {
   const { z, x, y } = req.query;
   const url = `https://maps.geoapify.com/v1/tile/osm-carto/${z}/${x}/${y}.png?apiKey=${process.env.GEOAPIFY_API_KEY}`;
+
   const response = await fetch(url);
+
+  if (!response.ok) {
+    res.status(response.status).end();
+    return;
+  }
+
   const buffer = await response.arrayBuffer();
   res.setHeader("Content-Type", "image/png");
+  res.setHeader("Cache-Control", "public, max-age=86400");
   res.send(Buffer.from(buffer));
 }
