864c4bb75090bf178341b330724da3a9424420d4
ASHIKA22R <rashika051@gmail.com>
2026-07-19 17:14:55 +0530

    commit change

--- script.js
+++ script.js
@@ -259,7 +259,12 @@ const data = await response.json();
             }
 
         }
-
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+    iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+    shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png"
+});
         marker = L.marker([lat, lon])
             .addTo(map)
             .bindPopup(`📍 ${place}`)
